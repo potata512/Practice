80a31a1ae5db0af63ddb87a1a5b8037b8b4ed9a2
potata512 <popota971@gmail.com>
2024-07-10 10:56:18 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+# ubuntu update
+
+apt-get update && apt-get -y upgrade
+apt install -y ca-certificates curl gnupg software-properties-common
+wget unzip apt-transport-https
+
+# Docker CLI
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o
+/etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+"deb [arch=$(dpkg --print-architecture) signedby=/etc/apt/keyrings/docker.asc]
+https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli
+
+# AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o
+"awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Terraform
+apt-get update && apt-get install -y gnupg software-propertiescommon
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archivekeyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt-get install -y terraform=1.6.6-1
+
+# Kubectl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key |
+gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg]
+https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | tee
+/etc/apt/sources.list.d/kubernetes.list
+apt-get update && apt-get install -y kubectl
+
+# kubens & kubectx
+git clone https://github.com/ahmetb/kubectx /opt/kubectx \
+&& ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx \
+&& ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
