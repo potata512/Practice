e3bf662b9c8c082504363e0939dd92dbc7a07857
potata512 <popota971@gmail.com>
2024-07-10 11:13:40 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ gpg --no-default-keyring \
 --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
 --fingerprint
 
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 tee /etc/apt/sources.list.d/hashicorp.list
 
 apt update && apt-get install -y terraform=1.6.6-1
