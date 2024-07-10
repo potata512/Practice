b3e1959d049559cba15e813166db3934d4cc8f43
potata512 <popota971@gmail.com>
2024-07-10 11:28:17 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,24 +15,4 @@ echo \
 apt-get update
 apt-get install -y docker-ce docker-ce-cli
 
-# AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-
-# Terraform
-RUN wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null  \
-    && gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint  \
-    && echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-
-# Kubectl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
-
-apt-get update && apt-get install -y kubectl
-# kubens & kubectx
-git clone https://github.com/ahmetb/kubectx /opt/kubectx  \
-    && ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx  \
-    && ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
